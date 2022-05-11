c838e3ee9865a3c1c2c152d084bdcdacbf215440
Zachary <me@dusterthefirst.com>
2022-05-11 15:09:44 +0200

    Add ultrasonc sensor

--- lib/drv_ultrasonic/ultrasonic.cpp
+++ lib/drv_ultrasonic/ultrasonic.cpp
@@ -1,12 +1,33 @@
 #include <Arduino.h>
 #include <ultrasonic.hpp>
 
-void ultrasonic::init() {
-    // TODO: Write initialization code
+// this constant won't change. It's the pin number of the sensor's output:
+#define PING_PIN 9
+
+uint64_t ultrasonic::distance() {
+    // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
+    // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+    pinMode(PING_PIN, OUTPUT);
+    digitalWrite(PING_PIN, LOW);
+    delayMicroseconds(2);
+    digitalWrite(PING_PIN, HIGH);
+    delayMicroseconds(5);
+    digitalWrite(PING_PIN, LOW);
+
+    // The same pin is used to read the signal from the PING))): a HIGH pulse
+    // whose duration is the time (in microseconds) from the sending of the ping
+    // to the reception of its echo off of an object.
+    pinMode(PING_PIN, INPUT);
+    noInterrupts();
+    uint64_t duration = pulseIn(PING_PIN, HIGH);
+    interrupts();
+
+    return microsecondsToCentimeters(duration);
 }
 
-float ultrasonic::distance() {
-    // TODO: Write code
-
-    return 0.0;
+uint64_t microsecondsToCentimeters(int64_t microseconds) {
+    // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+    // The ping travels out and back, so to find the distance of the object we
+    // take half of the distance travelled.
+    return microseconds / 29 / 2;
 }
--- lib/drv_ultrasonic/ultrasonic.hpp
+++ lib/drv_ultrasonic/ultrasonic.hpp
@@ -8,17 +8,9 @@
  */
 namespace ultrasonic {
     /**
-     * @brief Initialize the ultrasonic hardware.
-     *
-     * This function is called once on start up allowing for initialization
-     * code to prepare the attached hardware or to put it into a known state
-     */
-    void init();
-
-    /**
      * @brief Retrieve the distance measured by the ultrasonic sensor
      *
-     * @return The calculated distance in meters
+     * @return The calculated distance in whole centimeters
      */
-    float distance();
+    uint64_t distance();
 }
